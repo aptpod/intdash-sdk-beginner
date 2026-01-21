90d53227e34fb9a90a7a0d91188647feb85977b4
k-ise <k-ise@aptpod.co.jp>
2026-01-21 21:11:52 +0900

    データ名ハードコーディングを定数統合

---
+++ lesson9/src/const/const.py
@@ -0,0 +1,18 @@
+"""
+定数定義
+
+- 抽出データ名
+"""
+
+# 抽出データ名
+# Audio
+DATA_NAME_PCM = "1/pcm"
+DATA_NAME_AAC = "1/aac"
+
+# Video
+DATA_NAME_H264 = "1/h264"
+
+# GNSS / Subtitle
+DATA_NAME_ALTITUDE = "1/gnss_altitude"
+DATA_NAME_SPEED = "1/gnss_speed"
+DATA_NAME_COORDINATES = "1/gnss_coordinates"
